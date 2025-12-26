8c79439fea50386115a9fbb703f264b222d9229c
kaira <justaidajam@gmail.com>
2025-12-26 00:06:03 +0000

    #

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+ import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+  import { defineConfig } from 'vite'
+  import vue from '@vitejs/plugin-vue'
+  import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  // https://vite.dev/config/
+  export default defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-})
+    preview: {
+      port: 3042,
+      strictPort: true,
+      host: true,
+      allowedHosts: ['2next.semantyca.com'],
+    },
+  })
