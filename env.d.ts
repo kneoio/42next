6ce9fdeb22ab06f486215f5eb9a26edf1d2c3867
aidazi <biosubj@gmail.com>
2026-04-06 16:38:59 +0100

    #

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL?: string
+  readonly VITE_MIXPLA_SERVER?: string
   readonly VITE_KEYCLOAK_URL?: string
   readonly VITE_KEYCLOAK_REALM?: string
   readonly VITE_KEYCLOAK_CLIENT_ID?: string
